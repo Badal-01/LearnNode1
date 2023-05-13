b63fdf8b3ab5619c4e6e2d541419d3b12e3faf9d
Abhinandan kumar <badalcoder2018ee@gmail.com>
2023-05-13 22:23:11 +0530

    post method using mongoose

--- index.js
+++ index.js
@@ -1,37 +1,16 @@
-const mongoose = require("mongoose");
+const express = require('express');
+require('./config');
+const Products = require('./product');
 
-mongoose.connect("mongodb://localhost:27017/e-com");
 
-const productSchema = new mongoose.Schema({
-    name:String,
-    price:Number,
-    brand:String,
-    categoty:String,
-});
-
-async function saveInDb(){
-    const Product = mongoose.model('Product',productSchema);
-    let data = new Product({
-        name: "Note 6",
-        price:15000,
-        brand:"MI",
-        categoty:"Note",
-    })
-    const result = await data.save();
+const app = express();
+app.use(express.json());
+app.post("/create",async (req,res)=>{
+    let data = new Products(req.body);
+    let result = await data.save();
     console.log(result);
-}
+    res.send(result);
+    
+})
 
-saveInDb();
-
-const updateInDb = async () =>{
-    const Product = new mongoose.model("Product",productSchema);
-    let data = await Product.updateOne(
-        {name:"Note 6"},
-        {
-            $set:{price:18000}
-        }
-    );
-    console.log(data)
-}
-
-updateInDb();
+app.listen(5000);
