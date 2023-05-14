f09cfaeeebf93fe509c53adec3905cd7603f1a34
Abhinandan kumar <badalcoder2018ee@gmail.com>
2023-05-14 11:49:23 +0530

    update api

--- index.js
+++ index.js
@@ -12,5 +12,25 @@ app.post("/create",async (req,res)=>{
     res.send(result);
     
 })
+app.get("/list",async (req,res)=>{
+    let data = await Products.find();
+    console.log(data);
+    res.send(data);
+})
+app.delete("/delete/:_id",async (req,res)=>{
+    console.log(req.params);
+    let result = await Products.deleteOne(req.params)
+    res.send(result);
+})
+
+app.put("/update/:_id",async (req,res)=>{
+    console.log("Hiii")
+    console.log(req.params);
+    let result  = await Products.updateOne(
+        req.params,
+        {$set:req.body}
+    );
+    res.send(result);
+})
 
 app.listen(5000);
